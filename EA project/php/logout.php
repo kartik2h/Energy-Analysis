fb65ba6d2a16e50f7c7871332bc8d198cdd80b8f
kartik2h <50829311+kartik2h@users.noreply.github.com>
2019-06-14 13:29:23 +0530

    Add files via upload

---
+++ EA project/php/logout.php
@@ -0,0 +1,9 @@
+<?php
+session_destroy();
+echo'<head><script type = "text/javascript" >
+   function preventBack(){window.history.forward();}
+    setTimeout("preventBack()", 0);
+    window.onunload=function(){null};
+</script></head>';
+header("Location:http://localhost/EA%20project/Signin.php");
+?>
